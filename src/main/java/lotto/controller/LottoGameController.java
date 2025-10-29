641b9edb9d9f95c34ee8476a9c4342ed39b9dd05
Gammy98 <dnqorgh0410@naver.com>
2025-10-29 14:06:03 +0900

    feat: 컨트롤러 및 메인 애플리케이션 구현

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -1,4 +1,16 @@
 package lotto.controller;
 
+import lotto.view.InputView;
+
 public class LottoGameController {
+
+    private final InputView inputView;
+
+    public LottoGameController(){
+        this.inputView = new InputView();
+    }
+
+    public void run(){
+        int purchaseMoney = inputView.readPurchaseMoney();
+    }
 }
